8adf5d149e175a3c9a9989b5af0c08c27e2a736a
Kingsley Yong <kingsleyyong0208@gmail.com>
2024-06-15 16:32:00 +0800

    reset pretier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-export default function Home() {
-  return (
-    <>hello</>
-  );
+const Home = () => {
+      return (
+            <div className={'flex flex-col min-h-screen'}>
+                  <section className={'grow-0'}>
+                        <h1 className={'p-3'}>Single Page Application</h1>
+                  </section>
+                  <hr className={'mt-2'}/>
+                  <section className={'p-4 grow'}> tabel component </section>
+            </div>
+      )
 }
+
+export default Home;
